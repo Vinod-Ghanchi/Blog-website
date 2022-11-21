f457804b8528243bd53d9142cf3e9db0d66e9b81
Aryan <aryan.balpande@somaiya.edu>
2022-11-21 22:46:50 +0530

    commitsidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import "./home.css"
 import Header from "../../header/Header"
+import Posts from "../../posts/Posts"
+import Sidebar from "../../sidebar/Sidebar"
 
 export default function Home() {
   return (
+    <>
+            <Header/>
+
     <div className="home">
-        <Header/>
-        Homepage
+      
+        <Posts/>
+        <Sidebar/>
     </div>
+    </>
+
   )
 }
