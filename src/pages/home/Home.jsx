ab83970c3d28a5443156c78d477f3eb988f3eceb
Aryan <aryan.balpande@somaiya.edu>
2022-11-21 23:27:26 +0530

    commit second

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import "./home.css"
-import Header from "../../header/Header"
-import Posts from "../../posts/Posts"
-import Sidebar from "../../sidebar/Sidebar"
+import Header from "../../components/header/Header"
+import Posts from "../../components/posts/Posts"
+import Sidebar from "../../components/sidebar/Sidebar"
 
 export default function Home() {
   return (
