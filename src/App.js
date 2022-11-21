2844f799ccd575b831564ec35326df55130751f8
Aryan <aryan.balpande@somaiya.edu>
2022-11-22 01:02:08 +0530

    commit five

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Single from "./pages/home/single/Single";
 import Write from "./pages/home/write/Write";
 import Settings from "./pages/home/settings/Settings";
 import Login from "./pages/home/login/Login";
+import Register from "./pages/home/register/Register";
 function App() {
   return (
     <>
     <Topbar/>
-    <Login/>
+    <Register/>
     </>
   );
 }
