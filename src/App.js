705a15553903c7e12595fe31793c747d29f749fd
Aryan <aryan.balpande@somaiya.edu>
2022-11-22 00:16:43 +0530

    commit third

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import Single from "./pages/home/single/Single";
+import Write from "./pages/home/write/Write";
 
 function App() {
   return (
     <>
     <Topbar/>
-    <Home/>
+    <Write/>
     </>
   );
 }
