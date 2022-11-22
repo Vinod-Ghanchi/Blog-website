6a0a51ac051bf1aed802b3e270f7ef673e4000c8
Vinod-Ghanchi <ghanchivinod@gmail.com>
2022-11-23 00:44:32 +0530

    a

--- src/App.js
+++ src/App.js
@@ -9,26 +9,25 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 function App() { 
-  const user = false;
+  const user = true;
   return (
     <Router>
-    <Topbar/>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/register">{user ? <Home /> :<Register />}</Route>
-      <Route path="/login">{user ? <Home /> :<Login />}</Route>
-      <Route path="/write">{user ? <Write /> :<Register />}</Route>
-      <Route path="/settings">{user ? <Settings /> :<Register />}</Route>
-      <Route path="/post/:postId">
-        <Single />
-      </Route>
-    </Switch>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> :<Register />}</Route>
+        <Route path="/login">{user ? <Home /> :<Login />}</Route>
+        <Route path="/write">{user ? <Write /> :<Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> :<Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
     </Router>
   );
 }
