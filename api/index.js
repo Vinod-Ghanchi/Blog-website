242b590c0e16a8871645209aaa4d5b4667b589ca
Vinod-Ghanchi <ghanchivinod@gmail.com>
2022-11-23 15:13:25 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 
-app.use("/vinod",(req,res)=>{
-    console.log("Hey this is main URL")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,{
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
 })
+.then(console.log("Connected to MongoDB"))
+.catch(err=>console.log(err));
+
 
 app.listen("5000", ()=>{
     console.log("Backend is running");
