8cd4d70d7032534a6547be5f1757dfe416d36640
Vinod-Ghanchi <ghanchivinod@gmail.com>
2022-11-28 15:32:13 +0530

    finall

--- client/src/pages/About/about.jsx
+++ client/src/pages/About/about.jsx
@@ -4,13 +4,14 @@ export default function About() {
   return (
     <div className="about">
     Dear Guest,
+    <br /><br /><br /><br /><br />
     <p>
 Thank you for your continued support and the confidence that you have placed in us. 
 As we build the foundation of success, we completely recognize the trust you place in us
  and with your continuous support, we assure to maintain our best-in-class website.
-We are greatly humbled and honoured to be one of the top tourism blog sites.
-<h2>THE WORLD IS A BOOK AND THOSE WHO DO NOT TRAVEL READ ONLY ONE PAGE</h2>
-Yours Sincerely,
+We are greatly humbled and honoured to be one of the top tourism blog sites. <br /><br />
+<h2>THE WORLD IS A BOOK AND THOSE WHO DO NOT TRAVEL READ ONLY ONE PAGE</h2> <br />
+Yours Sincerely,<br />Aryan | Vinod | Kaushik
 </p>
     </div>
   );
